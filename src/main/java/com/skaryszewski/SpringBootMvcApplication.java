3b34513298c1fa1ac2903ed8fc1b2f1f70a8d349
Michal Skaryszewski <michal.skaryszewski@gmail.com>
2018-01-08 00:28:36 +0100

    updates index page

--- src/main/java/com/skaryszewski/SpringBootMvcApplication.java
+++ src/main/java/com/skaryszewski/SpringBootMvcApplication.java
@@ -8,5 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootMvcApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMvcApplication.class, args);
+		System.out.println("ASDASD");
 	}
 }
